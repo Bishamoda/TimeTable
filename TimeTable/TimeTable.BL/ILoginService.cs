a918f0f78dc2ec59ae742571d8a3b6ce3b0b8816
Bishamoda <mihailshipakin@mail.ru>
2022-05-19 20:00:51 +0300

    created login window

--- TimeTable/TimeTable.BL/ILoginService.cs
+++ TimeTable/TimeTable.BL/ILoginService.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace TimeTable.BL
 {
-    public class Class1
+    public  interface ILoginService
     {
+        bool CheckUser(string name, string password);
     }
 }
---
+++ TimeTable/TimeTable/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using TimeTable.DataAccess;
+
+namespace TimeTable
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+
+        }
+
+        public MainMenu(User user)
+        {
+            InitializeComponent();
+            monthCalendar1.CalendarDimensions = new System.Drawing.Size(1, 2);
+            label2.Text = String.Format($"{user.UserRole}, {user.UserName}");
+            label4.Text = String.Format($"{DateTime.UtcNow}");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+            using (LoginMenu loginMenu = new LoginMenu())
+            {
+                DialogResult dr = loginMenu.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    loginMenu.Close();
+                }
+            }
+        }
+    }
+}
